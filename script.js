9ab06a8eae37e73210e7e194a13c8e62a7e435e4
tommy <tcatty7@gmail.com>
2022-08-10 23:53:04 -0400

    g

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const myTimeout = setTimeout(myGreeting, 10000);
 function myGreeting() {
   document.getElementById("container").style.display = "none"
 }
+const myTimeout2 = setTimeout(myGreeting2, 11000);
+
+function myGreeting2() {
+  document.getElementById("typer").style.display = "inherit"
+}
+const myTimeout1 = setTimeout(myGreeting1, 20000);
+
+function myGreeting1() {
+  document.getElementById("typer").style.display = "none"
+}
